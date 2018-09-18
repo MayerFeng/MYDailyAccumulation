059b0ce869038ec97d5bd53af1f9aed1d5f159bd
MayerFeng <Queenmayer@163.com>
2018-09-18 16:31:52 +0800

    Initial Commit

---
+++ MYDailyAccumulation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYDailyAccumulation
+//
+//  Created by Mayer on 2018/9/18.
+//  Copyright © 2018年 TJMYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYDailyAccumulation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYDailyAccumulation
+//
+//  Created by Mayer on 2018/9/18.
+//  Copyright © 2018年 TJMYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
